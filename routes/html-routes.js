fa2d5dcd50537a885c615691e7eb47ca832779d6
David Dugle <david.dugleii@yahoo.com>
2020-12-18 22:52:49 -0500

    minor changes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -25,7 +25,7 @@ module.exports = function(app) {
       let dataValues;
       if (req.user.clearance) {
         res.render("manager", { email: req.user.email });
-      } else if (dataValues !== undefined) {
+      } else if ((dataValues === void 0)) {
         const orders = await user.getOrders();
         console.log(orders[0].dataValues);
         const orderList = orders.map(order => {
